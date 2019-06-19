37899800587a052023cc95600aee538c79c75eec
Hearty Oh <heartyoh@hatiolab.com>
2019-06-19 18:02:15 +0900

    v0.0.4

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -3,12 +3,10 @@ import { store, UPDATE_BASE_URL, UPDATE_DEFAULT_ROUTE_PAGE } from '@things-facto
 export default function bootstrap() {
   store.dispatch({
     type: UPDATE_BASE_URL
-    // baseUrl: 'http://52.231.75.202/rest'
-    // baseUrl: 'http://board-demo.hatiolab.com/rest'
   })
 
   store.dispatch({
     type: UPDATE_DEFAULT_ROUTE_PAGE,
-    defaultRoutePage: 'menu-list'
+    defaultRoutePage: 'board-list'
   })
 }
