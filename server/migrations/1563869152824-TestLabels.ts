aaab5d16a1a46a35e75572a6c396be8b77b9459d
Hearty Oh <heartyoh@hatiolab.com>
2019-07-23 17:08:02 +0900

    Create 1563869152824-TestLabels.ts

---
+++ server/migrations/1563869152824-TestLabels.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+import { getRepository } from 'typeorm'
+import { Domain } from '@things-factory/shell'
+import { Group, Board } from '@things-factory/board-base'
+
+const SEED_BOARD = []
+
+export class TestLabels1558006512362 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    const repository = getRepository(Board)
+    const domainRepository = getRepository(Domain)
+    const domain = await domainRepository.findOne({ name: 'SYSTEM' })
+
+    const groupRepository = getRepository(Group)
+    const group = await groupRepository.findOne({ name: 'SAMPLE' })
+
+    try {
+      SEED_BOARD.forEach(async board => {
+        await repository.save({
+          ...board,
+          domain,
+          group
+        })
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    const repository = getRepository(Board)
+
+    SEED_BOARD.reverse().forEach(async board => {
+      let record = await repository.findOne({ name: board.name })
+      await repository.remove(record)
+    })
+  }
+}
